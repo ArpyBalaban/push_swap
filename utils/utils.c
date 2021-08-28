d3b6adc54355be8eaedc2e4e5cc35845a1fd582f
ArpyBalaban <arpy.balaban@tumo.org>
2021-08-28 22:21:45 +0400

    added functions get_distance and free_stack

--- utils/utils.c
+++ utils/utils.c
@@ -24,11 +24,43 @@ int is_sorted(t_list** stack)
 	t_list* head;
 
 	head = *stack;
-	while (head)
+	while (head && head->next)
 	{
 		if (head->value > head->next->value)
 			return (0);
 		head = head->next;
 	}
 	return (1);
+}
+
+void	free_stack(t_list** stack)
+{
+	t_list	*head;
+	t_list	*tmp;
+
+	head = *stack;
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	free(stack);
+}
+
+int	get_distance(t_list **stack, int index)
+{
+	t_list	*head;
+	int		distance;
+
+	distance = 0;
+	head = *stack;
+	while (head)
+	{
+		if (head->index == index)
+			break ;
+		distance++;
+		head = head->next;
+	}
+	return (distance);
 }
