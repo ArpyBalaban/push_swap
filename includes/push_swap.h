e96ed67dbbcde19ce16def035b1d9ff41a04109a
ArpyBalaban <arpy.balaban@tumo.org>
2021-08-28 22:05:08 +0400

    added operations, utils and simple_sort

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -22,4 +22,25 @@ int		ft_lstsize(t_list **stack);
 void	ft_lstprint(t_list **stack);
 void	index_stack(t_list **stack);
 int		is_sorted(t_list** stack);
+void	free_stack(t_list** stack);
+int		get_distance(t_list **stack, int index);
+int		simple_sort(t_list** stack_a, t_list** stack_b);
+
+int		swap(t_list** stack);
+int		push(t_list** stack_to, t_list** stack_from);
+int		rotate(t_list **stack);
+int		reverseRotate(t_list **stack);
+
+int		sa(t_list **stack_a);
+int		sb(t_list **stack_b);
+int		ss(t_list **stack_a, t_list **stack_b);
+int		pa(t_list **stack_a, t_list **stack_b);
+int		pb(t_list **stack_b, t_list **stack_a);
+int		ra(t_list **stack_a);
+int		rb(t_list **stack_b);
+int		rr(t_list **stack_a, t_list **stack_b);
+int		rra(t_list **stack_a);
+int		rrb(t_list **stack_b);
+int		rrr(t_list **stack_a, t_list **stack_b);
+
 #endif
